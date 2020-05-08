c528f96a944c4ed360d12337849f2dde836eef06
Felipe Ferreira <felipe.ferreira0192@gmail.com>
2020-05-08 09:15:06 -0300

    test com getmapping, requestmapping, requestparam e pathvariable

--- src/main/java/github/io/forum/ForumApplication.java
+++ src/main/java/github/io/forum/ForumApplication.java
@@ -2,12 +2,26 @@ package github.io.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/hello")
 public class ForumApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
 	}
+
+	@GetMapping("/test")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+//	outra forma de ser usado
+//	@GetMapping("/test/{name}")
+//	public String hello(@PathVariable("name") String name) {
+//		return String.format("Hello %s!", name);
+//	}
 
 }
