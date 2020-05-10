fa4d268067cd61365b925ed7c8535cde7b7ffe9d
Felipe Ferreira <felipe.ferreira0192@gmail.com>
2020-05-10 13:43:16 -0300

    put e delete do answer OK

--- src/main/java/github/io/forum/rest/controller/AnswerController.java
+++ src/main/java/github/io/forum/rest/controller/AnswerController.java
@@ -7,6 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/answer")
 public class AnswerController {
@@ -18,7 +21,34 @@ public class AnswerController {
     public ResponseEntity saveAnswer(@RequestBody AnswerEntity answer) {
         AnswerEntity answerSaved = answerRepository.save(answer);
 
-        return ResponseEntity.ok(HttpStatus.CREATED);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable Integer id){
+        Optional<AnswerEntity> answer = answerRepository.findById(id);
+
+        if(answer.isPresent()) {
+            answerRepository.delete(answer.get());
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AnswerEntity> update(@PathVariable Integer id, @Valid @RequestBody AnswerEntity newAnswer){
+        Optional<AnswerEntity> answerExistente = answerRepository.findById(id);
+
+        if (answerExistente.isPresent()) {
+            AnswerEntity answer = answerExistente.get();
+            answer.setContent(newAnswer.getContent());
+            answerRepository.save(answer);
+            return new ResponseEntity<AnswerEntity>(answer, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
     }
 
 }
